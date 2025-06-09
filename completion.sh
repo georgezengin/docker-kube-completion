7311f8c4b7fdb47ab789f30d4cbe7f71f1603561
George Zengin <george.zengin@gmail.com>
2025-06-09 03:17:32 +0300

    Add completion_keywords.ini file and functionality

--- completion.sh
+++ completion.sh
@@ -1,16 +1,56 @@
 #!/bin/bash
 
+# Path to the keyword file
+INI_FILE="$HOME/.docker-kube-completion/completion_keywords.ini"
+
+# Default hardcoded fallbacks
+default_docker_cmds="build commit cp create diff events exec export history images import info inspect kill load login logout logs pause port ps pull push rename restart rm rmi run save search start stats stop tag top unpause update version volume"
+default_docker_flags="--help --verbose --tls --tlsverify --config"
+
+default_kube_cmds="apply attach auth autoscale certificate cluster-info completion config convert cordon cp create delete describe drain edit exec explain expose get label logs patch plugin port-forward proxy replace rollout run scale set top uncordon version"
+default_kube_flags="--namespace -n --context --kubeconfig --help -o"
+
+default_helm_cmds="install uninstall list upgrade repo search show create lint test rollback history pull template package plugin"
+default_compose_cmds="build up down logs restart start stop exec run config ps top pull push"
+
+# Read comma-separated values from an ini section into space-separated strings
+read_ini_values() {
+    local section="$1"
+    local key="$2"
+    awk -F= -v section="[$section]" -v key="$key" '
+        $0 == section { found=1; next }
+        /^\[.*\]/     { found=0 }
+        found && $1 ~ key {
+            gsub(/[ \t]/, "", $2)
+            gsub(/,/, " ", $2)
+            print $2
+            exit
+        }
+    ' "$INI_FILE"
+}
+
+# Load keyword lists with fallback
+docker_cmds=$(read_ini_values docker commands)
+docker_cmds="${docker_cmds:-$default_docker_cmds}"
+
+docker_flags=$(read_ini_values docker flags)
+docker_flags="${docker_flags:-$default_docker_flags}"
+
+kube_cmds=$(read_ini_values kubectl commands)
+kube_cmds="${kube_cmds:-$default_kube_cmds}"
+
+kube_flags=$(read_ini_values kubectl flags)
+kube_flags="${kube_flags:-$default_kube_flags}"
+
+helm_cmds=$(read_ini_values helm commands)
+helm_cmds="${helm_cmds:-$default_helm_cmds}"
+
+compose_cmds=$(read_ini_values compose commands)
+compose_cmds="${compose_cmds:-$default_compose_cmds}"
+
 _docker_kube_custom_complete() {
     local cur prev words cword
     _init_completion || return
-
-    local docker_cmds="build commit cp create diff events exec export history images import info inspect kill load login logout logs pause port ps pull push rename restart rm rmi run save search start stats stop tag top unpause update version volume"
-    local docker_flags="--help --verbose --tls --tlsverify --config"
-    local kube_cmds="apply attach auth autoscale certificate cluster-info completion config convert cordon cp create delete describe drain edit exec explain expose get label logs patch plugin port-forward proxy replace rollout run scale set top uncordon version"
-    local kube_flags="--namespace -n --context --kubeconfig --help -o"
-
-    local helm_cmds="install uninstall list upgrade repo search show create lint test rollback history pull template package plugin"
-    local compose_cmds="build up down logs restart start stop exec run config ps top pull push"
 
     case "${COMP_WORDS[0]}" in
         docker)
@@ -54,6 +94,7 @@ _docker_kube_custom_complete() {
             ;;
     esac
 }
+
 complete -F _docker_kube_custom_complete docker
 complete -F _docker_kube_custom_complete kubectl
 complete -F _docker_kube_custom_complete helm
